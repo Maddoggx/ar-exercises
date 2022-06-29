ba944209d77cf9ffafa60aecf7b27aa32d2a7df5
Amera Alleyne <amera.alleyne@gmail.com>
2022-06-28 17:30:11 -0700

    ex.5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+@average_revenue = @total_revenue / @count_store
+@count_1m_revenue = Store.where('annual_revenue >= ?', 10000).count
+
+puts @total_revenue
+puts @average_revenue
+puts @count_1m_annual
